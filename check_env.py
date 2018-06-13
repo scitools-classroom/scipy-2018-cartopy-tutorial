9d3e01f5f0554d283e4a824950a45b7e1fe08456
pelson <pelson.pub@gmail.com>
2018-06-13 12:30:05 +0100

    Add outline of tutorial.

---
+++ check_env.py
@@ -0,0 +1,38 @@
+"""
+Test import of packages for the tutorial
+
+"""
+from __future__ import print_function
+
+import importlib
+import sys
+
+
+def version(pkg):
+    try:
+        mod = importlib.import_module(pkg)
+        print(OK, '%s version %s' % (pkg, mod.__version__))
+    except ImportError:
+        print(FAIL, '%s not installed' % pkg)
+
+try:
+    import curses
+    curses.setupterm()
+    assert curses.tigetnum("colors") > 2
+    OK = "\x1b[1;%dm[ OK ]\x1b[0m" % (30 + curses.COLOR_GREEN)
+    FAIL = "\x1b[1;%dm[FAIL]\x1b[0m" % (30 + curses.COLOR_RED)
+except:
+    OK = '[ OK ]'
+    FAIL = '[FAIL]'
+
+print('Using python in', sys.prefix)
+print(sys.version)
+
+print()
+print('Required packages:')
+
+version('cartopy')
+version('iris')
+version('numpy')
+version('shapely')
+print()
