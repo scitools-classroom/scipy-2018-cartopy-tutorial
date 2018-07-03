3b4373074ddc4b740e4192921a3d6f5e6926a2e9
pelson <pelson.pub@gmail.com>
2018-07-03 12:02:51 +0100

    Added two exersizes to georeference the Wikipedia image.
    Also improved the theming of the introduction.

--- appendix/image_geolocator.py
+++ appendix/image_geolocator.py
@@ -21,10 +21,12 @@ ax.coastlines()
 
 extent = list(rob.x_limits) + list(rob.y_limits)
 # A slightly better approximation...
-# extent = [-13699119.4302052, 17123551.559067532, -6356356.797292399, 8605867.777293697]
+# extent = [-13699119.4302052, 17123551.559067532,
+#           -6356356.797292399, 8605867.777293697]
 
-img = plt.imread('../resources/640px-Around_the_World_in_Eighty_Days_map.png')
-img = ax.imshow(img, extent=extent, transform=rob, origin='upper')
+img_array = plt.imread(
+    '../resources/640px-Around_the_World_in_Eighty_Days_map.png')
+img = ax.imshow(img_array, extent=extent, transform=rob, origin='upper')
 
 # Create a separate axes for the Rectangle selector (as it needs
 # to extend beyond the axes clip area).
@@ -33,24 +35,26 @@ ax_selector.patch.set_visible(False)
 
 
 def transform_extent(extent, transform):
-    # A quick and dirty transformation of an extent, rather than a bbox.
+    # A quick and dirty matplotlib transformation of an extent,
+    # rather than a bbox.
     x0, y0 = transform.transform_point([extent[0], extent[2]])
     x1, y1 = transform.transform_point([extent[1], extent[3]])
     return x0, x1, y0, y1
 
 
 def line_select_callback(eclick, erelease):
+    # Update the image extent when the rectangle selector is moved.
     selector_to_rob = ax_selector.transData + ax.transData.inverted()
     ext = transform_extent(selector.extents, selector_to_rob)
     print('New extents: [{}, {}, {}, {}]'.format(*ext))
     img.set_extent(ext)
 
 
-selector = RectangleSelector(ax_selector, line_select_callback,
-                                       drawtype='box',
-                                       minspanx=5, minspany=5,
-                                       spancoords='pixels',
-                                       interactive=True)
+selector = RectangleSelector(
+    ax_selector, line_select_callback,
+    drawtype='box', minspanx=5, minspany=5,
+    spancoords='pixels', interactive=True)
+
 
 def align_box_to_img(event):
     # Because cartopy preserves aspect ratio, we need to trigger this
@@ -59,7 +63,6 @@ def align_box_to_img(event):
     selector.extents = transform_extent(extent, rob_to_selector)
     plt.draw()
 cid = ax.figure.canvas.mpl_connect('resize_event', align_box_to_img)
-
 align_box_to_img(None)
 
 plt.show()
--- static/theme/reveal.js
+++ static/theme/reveal.js
@@ -3,11 +3,13 @@
 $('head').append('<link rel="stylesheet" href="/files/static/theme/rise.css" type="text/css" />');
 
 header = `
-<img src="https://scitools.org.uk/static/images/logo.200x200-trans-white-122x122.png" style="height: 3.8rem;" />
-SciTools
+<a href="https://scitools.org.uk">
+    <img src="https://scitools.org.uk/static/images/logo.200x200-trans-white-122x122.png" style="height: 4.5rem;" />
+    SciTools
+</a>
 <div style="float: right;">
-  <img src="/files/static/theme/favicon.png" style="height: 3.8rem;">
-  Cartopy tutorial | SciPy 2018
+    <img src="/files/static/theme/favicon.png" style="height: 3.8rem;">
+    Cartopy tutorial | SciPy 2018
 </div>
 `;
 
@@ -20,3 +22,20 @@ footer = `
 
 $('#rise-header').html(header);
 $('#rise-footer').html(footer);
+
+
+index = Reveal.getIndices();
+if (index.h == 0 & index.v == 0) {
+    onFirstSlide();
+}
+
+Reveal.addEventListener('slidechanged', function(evt) {
+  if (evt.indexh == 0 & evt.indexv == 0) {
+    onFirstSlide();
+  }
+});
+
+
+function onFirstSlide() {
+  // Do the thing when first slide is being shown.
+}
